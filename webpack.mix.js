da4216e98d3d46865bd40f69cb9a748fda2b5e76
Igor Jozefowicz <igorjozefowicz@gmail.com>
2022-09-25 20:26:14 +0200

    publish AdminLTE plublic files

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy('node_modules/admin-lte/dist/img', 'public/dist/img');
 
 if (mix.inProduction()) {
     mix.sourceMaps()
