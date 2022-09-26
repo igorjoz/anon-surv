0df0b45ae77e2d303916e148749c0f8871072ecd
Igor Jozefowicz <igorjozefowicz@gmail.com>
2022-09-26 21:38:22 +0200

    create components: crud & button, form.error-box
    - upload public assets

---
+++ resources/views/components/form/error-box.blade.php
@@ -0,0 +1,7 @@
+@if ($errors->any())
+<div class="crud__error-box">
+  <span class="crud__error crud__error--big">
+    Błędnie uzupełniony formularz, sprawdź błędy poniżej
+  </span>
+</div>
+@endif
